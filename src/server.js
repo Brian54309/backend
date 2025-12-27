e538d5d58c6acbb34560f4b4ac0b288e3aaaaa49
brian budiarto <brianbudiarto@gmail.com>
2025-12-27 10:12:45 +0700

    update port

--- src/server.js
+++ src/server.js
@@ -5,8 +5,8 @@ import app from "./app.js";
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () =>
-  console.log(`Backend running on port ${PORT}`)
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Backend running on 0.0.0.0:${PORT}`);
+});
